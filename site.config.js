585028d674bd7333fc93e200c95f4d656cd2c638
Pgao4 <93553632+Pgao4@users.noreply.github.com>
2022-01-22 10:29:10 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'fe4e980ab6fb4bd88468812206ca9271',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Kyro Leetcode Record',
+  domain: 'https://charmed-billboard-d36.notion.site/fe4e980ab6fb4bd88468812206ca9271?v=181c927af71e4395b47892874a7c4e47',
+  author: 'Kyro Gao',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'pgao4',
+  linkedin: 'KyroGao',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
